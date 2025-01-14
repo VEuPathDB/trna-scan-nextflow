ca2b444ae25a161c5825d015162a525ef5200493
John Brestelli <jbrestell@gmail.com>
2025-01-13 22:36:34 -0500

    set temp_dir in process dir

--- modules/templates/runtRNAScan.bash
+++ modules/templates/runtRNAScan.bash
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-tRNAscan-SE $subsetFasta \
-  -o subset.scanned 
+
+mkdir ./trnascanTmp
+
+if [ -f "/usr/bin/tRNAscan-SE.conf" ]; then
+    perl -e 'while(<>) {if(/^temp_dir:/) {print "temp_dir: trnascanTmp\n"} else {print}}' /usr/bin/tRNAscan-SE.conf  >tRNAscan-SE_mod.conf
+    tRNAscan-SE $subsetFasta -o subset.scanned --conf tRNAscan-SE_mod.conf
+else
+  echo "ERROR:  /usr/bin/tRNAScan-SE.conf file does not exist"
+  exit 1
+fi
